16c121d7eba8bf3f1599218a918138273224a691
Keyur Prajapati <129269391+keyur97@users.noreply.github.com>
2023-08-02 03:16:09 +0100

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    $('.hamburger').click(function() {
+        $('.left_sidebar').css({
+            'transform': 'translateX(0)'
+        });
+    });
+    $('.student').click(function() {
+        $('.right_sidebar').css({
+            'transform': 'translateX(0)'
+        });
+    });
+
+    $('.close_btn').click(function() {
+        $('.right_sidebar').css({
+            'transform': 'translateX(150%)'
+        })
+    })
+
+    $('.close_hamburger_btn').click(function() {
+        $('.left_sidebar').css({
+            'transform': 'translateX(-150%)'
+        })
+    })
+
+});
+
+function darkMode() {
+    $('body').toggleClass('dark-mode');
+    $('.table tr:nth-child(even)').css({
+        'color': '#000',
+    })
+    $('.table tr:nth-child(4)').css({
+        'background-color': '#5bb9c0',
+        'color': '#fff',
+    })
+
+    $('.main_content .main_navbar .dark_mode_icon .bx-sun').click(function() {
+        $(this).css('display', 'none');
+        $('.main_content .main_navbar .dark_mode_icon .bx-moon').css('display', 'block');
+    });
+
+    $('.main_content .main_navbar .dark_mode_icon .bx-moon').click(function() {
+        $(this).css('display', 'none');
+        $('.main_content .main_navbar .dark_mode_icon .bx-sun').css('display', 'block');
+    });
+}
+
+function deleteBook(bookid) {
+    if(confirm("Are you sure want to delete book?")){
+         const formData = new FormData();
+         formData.append('bookid', bookid);
+     fetch('/book', {
+         method: 'DELETE',
+         body: formData
+     }).then(response => response.json())
+     window.location.reload()
+    }
+    
+ }
